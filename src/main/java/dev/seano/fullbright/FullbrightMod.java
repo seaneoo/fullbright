660690ec95a03cf5db5eb2aa8149048204df890c
Sean O'Connor <seanoconnor941@gmail.com>
2020-07-12 17:35:20 -0600

    1.16.1-0.3.0

--- src/main/java/dev/seano/fullbright/FullbrightMod.java
+++ src/main/java/dev/seano/fullbright/FullbrightMod.java
@@ -1,12 +1,13 @@
 package dev.seano.fullbright;
 
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.network.MessageType;
 import net.minecraft.text.TranslatableText;
 import org.lwjgl.glfw.GLFW;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.api.event.client.ClientTickCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil;
@@ -16,67 +17,68 @@ import java.util.TimerTask;
 
 public class FullbrightMod implements ClientModInitializer {
 
-    private MinecraftClient minecraftClient;
-    private final String KEYBIND_CATEGORY = "key.fullbright.category";
-    private final KeyBinding TOGGLE_FULLBRIGHT_KEYBIND = new KeyBinding("key.fullbright.toggle_fullbright",
-            InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_H, KEYBIND_CATEGORY);
-    private final double modGamma = 10.0D;
-    private boolean enabled = false;
+   private MinecraftClient minecraftClient;
+   private final String KEYBIND_CATEGORY = "key.fullbright.category";
+   private final KeyBinding TOGGLE_FULLBRIGHT_KEYBIND = new KeyBinding("key.fullbright.toggle_fullbright",
+           InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_H, KEYBIND_CATEGORY);
+   private boolean enabled = false;
+   private final double modGamma = 10.0D;
 
-    @SuppressWarnings("deprecation")
-    @Override
-    public void onInitializeClient() {
-        ModConfig.getConfig().load();
+   @Override
+   public void onInitializeClient() {
+      ModConfig.getConfig().load();
 
-        minecraftClient = MinecraftClient.getInstance();
-        KeyBindingHelper.registerKeyBinding(TOGGLE_FULLBRIGHT_KEYBIND);
+      minecraftClient = MinecraftClient.getInstance();
+      KeyBindingHelper.registerKeyBinding(TOGGLE_FULLBRIGHT_KEYBIND);
 
-        ClientTickCallback.EVENT.register(client -> {
-            while (TOGGLE_FULLBRIGHT_KEYBIND.wasPressed()) {
-                if (!enabled) {
-                    enabled = true;
-                    increase();
-                    client.inGameHud.addChatMessage(MessageType.GAME_INFO,
-                            new TranslatableText("chat.fullbright_enabled"), null);
-                } else {
-                    enabled = false;
-                    decrease();
-                    client.inGameHud.addChatMessage(MessageType.GAME_INFO,
-                            new TranslatableText("chat.fullbright_disabled"), null);
-                }
+      ClientLifecycleEvents.CLIENT_STARTED.register(client -> enabled = !(minecraftClient.options.gamma <= 1.0));
+
+      ClientTickEvents.START_CLIENT_TICK.register(client -> {
+         while (TOGGLE_FULLBRIGHT_KEYBIND.wasPressed()) {
+            if (!enabled) {
+               enabled = true;
+               increase();
+               client.inGameHud.addChatMessage(MessageType.GAME_INFO,
+                       new TranslatableText("chat.fullbright_enabled"), null);
+            } else {
+               enabled = false;
+               decrease();
+               client.inGameHud.addChatMessage(MessageType.GAME_INFO,
+                       new TranslatableText("chat.fullbright_disabled"), null);
             }
-        });
-    }
+         }
+      });
+   }
 
-    private void increase() {
-        Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new TimerTask() {
-            double d = ModConfig.getConfig().getDefaultGamma();
+   private void increase() {
+      Timer timer = new Timer();
+      timer.scheduleAtFixedRate(new TimerTask() {
+         double d = ModConfig.getConfig().getDefaultGamma();
 
-            @Override
-            public void run() {
-                if (d > modGamma || !enabled) timer.cancel();
-                else {
-                    d += 0.5;
-                    minecraftClient.options.gamma = d;
-                }
+         @Override
+         public void run() {
+            if (d >= modGamma || !enabled) timer.cancel();
+            else {
+               d += 0.5;
+               minecraftClient.options.gamma = d;
             }
-        }, 0L, 10L);
-    }
+         }
+      }, 0L, 10L);
+   }
 
-    private void decrease() {
-        Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new TimerTask() {
-            double d = modGamma;
+   private void decrease() {
+      Timer timer = new Timer();
+      timer.scheduleAtFixedRate(new TimerTask() {
+         double d = modGamma;
 
-            @Override
-            public void run() {
-                if (d < ModConfig.getConfig().getDefaultGamma() || enabled) timer.cancel();
-                else {
-                    d -= 0.5;
-                    minecraftClient.options.gamma = d;
-                }
+         @Override
+         public void run() {
+            if (d <= ModConfig.getConfig().getDefaultGamma() || enabled) timer.cancel();
+            else {
+               d -= 0.5;
+               minecraftClient.options.gamma = d;
             }
-        }, 0L, 10L);
-    }
+         }
+      }, 0L, 10L);
+   }
 }
